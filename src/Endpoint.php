a15769aaa0a33e421c29da941f998c2cc2f0f4de
sergei.baikin <sergei.baikin@fotograf.de>
2024-09-24 14:24:25 +0200

    Add prefix

--- src/Endpoint.php
+++ src/Endpoint.php
@@ -9,34 +9,34 @@ namespace Gpht\ExcimerS3;
  */
 final class Endpoint
 {
-    public static function web(string $dsnString): void
+    public static function web(string $dsnString, string $prefix = '/'): void
     {
         if ((isset($_GET['profile']) || isset($_GET['timer'])) && extension_loaded('excimer')) {
             $excimer = ExcimerS3::ofDsn($dsnString);
             if (isset($_GET['profile']) && is_string($_GET['profile'])) {
                 $path = str_replace('__', '/', $_GET['profile']);
-                $excimer->trace($path);
+                $excimer->trace($prefix . '/' . $path);
             }
             if (isset($_GET['timer']) && is_string($_GET['timer'])) {
                 $path = str_replace('__', '/', $_GET['timer']);
-                $excimer->timer($path);
+                $excimer->timer($prefix . '/' . $path);
             }
         }
     }
 
-    public static function cli(string $dsnString): void
+    public static function cli(string $dsnString, string $prefix = '/'): void
     {
         if (extension_loaded('excimer') && ((getenv("PROFILE") !== false) || (getenv("TIMER") !== false))) {
             $excimer = ExcimerS3::ofDsn($dsnString);
             $profilePath = getenv("PROFILE");
             if (is_string($profilePath)) {
                 $path = str_replace('__', '/', $profilePath);
-                $excimer->trace($path);
+                $excimer->trace($prefix . '/' . $path);
             }
             $timerPath = getenv("TIMER");
             if (is_string($timerPath)) {
                 $path = str_replace('__', '/', $timerPath);
-                $excimer->timer($path);
+                $excimer->timer($prefix . '/' . $path);
             }
         }
     }
